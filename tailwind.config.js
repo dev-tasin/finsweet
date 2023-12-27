8d6fe4f2bb1c5f2e461448ba018d2eb12949f53a
dev-tasin <ahmedtasin94@gmail.com>
2023-12-28 01:45:31 +0600

    80% done

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#FFD2A4',
+          navbg: '#0D0D0D',
+          navbtn:'#1C1D28',
+          subheading:'#161722',
+          servicebg:'#FFF5EB',
+          watchbg:'#F5F2F0',
+        },
+        maxWidth: {
+            'container': '1280px',
+          },
+        fontFamily: {
+            'roboto': "Roboto Condensed, sans-serif;",
+          },
+        backgroundImage: {
+            'banner': "url('images/banner.jpg')",
+            overlay: "linear-gradient(60deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%)",
+            textgradient: "linear-gradient(110deg, #A54E2B -53.83%, #DC9853 62.61%)",
+            'visitbg': "url('images/visit.jpg')",
+          }
+      }
+    }
+  }
